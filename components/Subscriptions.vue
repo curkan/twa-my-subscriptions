b0fea3db943cb26ff8d0685b18a754626cce166b
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2024-11-26 21:44:29 +0300

    SCR #0 - remove block switch

--- components/Subscriptions.vue
+++ components/Subscriptions.vue
@@ -69,15 +69,6 @@ const openModalDeleting = (
 </script>
 
 <template>
-  <div>
-    <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="sepia">Sepia</option>
-    </select>
-  </div>
     <UModal v-model="isOpenEdit">
       <div class="p-4">
         <Placeholder class="h-48" />
