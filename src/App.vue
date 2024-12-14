8544804bd8ac4fec4d355e40a4e9d3c0f6ae7e84
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2024-12-14 09:37:51 +0300

    SCR #0 - fix router transactions

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import { useThemeStore } from "./stores/themeStore";
 </script>
 
 <template>
   <van-config-provider :theme="useThemeStore().currentTheme" />
-  <transition>
-    <RouterView />
-  </transition>
+    <router-view v-slot="{ Component, route }">
+      <transition mode="out-in">
+        <div :key="route.name">  
+          <component :is="Component"></component>
+        </div>
+      </transition>
+    </router-view>
 </template>
 
 <style scoped></style>
